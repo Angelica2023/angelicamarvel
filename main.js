fb918a951e75e15effa664929b8ff3dc5a4ab554
Norma Angelica <angelica.alp@gmail.com>
2023-05-26 02:16:44 -0600

    Avance 70% falta cargar la imagen. Revisar la carga de imagenes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import {
-     buscandoLosMarvel,
-    } from "./marvel.js";
+     buscandoLosMarvel, buscarMarvelporNombre  } from "./marvel.js";
 
 
 
@@ -11,11 +10,52 @@ const marveles = await buscandoLosMarvel()
 //console.log (marveles)
 
 for (let i = 0; i < marveles.length; i++) {
-    const marvel = marveles[i]; 
-console.log ("El número de personaje marvel es",i, marvel);
-//root.innerHTML += `<div class="tarjeta">${marvel.description}</div>`;
+    const marvel = marveles[i]; //i es la variable que cambia
+   root.innerHTML += `<div class="tarjeta">Nombre:${marvel.name}
+ <div class="tarjeta-content">
+Numero:${marvel.numero}
+<div class="tarjeta-content">
+<span> Descripción: ${marvel.descripcion.slice(0, 20)}...</span>
 
+ Imágen: <img scr=${marvel.imagen.path}.${marvel.imagen.extension} alt="${marvel.name}">
+ 
+
+</div>
+</div>`;
 }
 }
 
 mostrarMarveles()
+
+const buscador = document.getElementById("buscador"); //id de index.html
+
+buscador.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  root.innerHTML ="Espera un momento, buscando entre los Marvel"
+
+const NombreDelMarvel = document.getElementById("input-buscar").value;
+
+const marveles = await buscarMarvelporNombre(NombreDelMarvel);
+
+root.innerHTML="";
+
+
+
+for (let i = 0; i < marveles.length; i++) {
+  const marvel = marveles[i]; 
+ 
+  root.innerHTML += `<div class="tarjeta">Nombre:${marvel.name}
+ <div class="tarjeta-content">
+
+Numero:${marvel.numero}
+<div class="tarjeta-content">
+<span> Descripción: ${marvel.descripcion}...</span>
+Imágen: <img scr=${marvel.imagen}/>
+
+
+</div>
+</div>
+</div>`;
+}
+});
--- marvel.js
+++ marvel.js
@@ -1,29 +1,60 @@
 export async function buscandoLosMarvel() {
-  try {
-    //llamada a la API con un limite de 200
-    const response = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=89d89bbcc96a5518b7045154acab2cd0&hash=bcd2343e53b65e1753264d81de0fa5f3&limit=30");
+ try {
+    
+    const response = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=89d89bbcc96a5518b7045154acab2cd0&hash=bcd2343e53b65e1753264d81de0fa5f3&limit=50");
+   
+    
     const data = await response.json();
     
     const resultadosObtenidos = data.data.results;
   
     const resultadosFormateados = [];
-  
-    resultadosObtenidos.forEach((personaje) => {
+    resultadosObtenidos.forEach((marvel) => {
       let personajeFormateado = {
-        thumbnail: personaje.thumbnail,
-        name: personaje.name,
-        description: personaje.description
+        imagen: marvel.thumbnail,
+        name: marvel.name,
+        numero: marvel.id,
+        descripcion: marvel.description,
 
       };
       resultadosFormateados.push(personajeFormateado);
     });
-  
-    console.log(resultadosFormateados);
+     console.log(resultadosFormateados);
     return resultadosFormateados;
-  } catch (error) {
-    console.error('Error al obtener los personajes:', error);
+ } catch (error) {
+     console.error('Error al obtener los personajes:', error);
     return [];
-  }
+   }
 }
 
-buscandoLosMarvel();
+ buscandoLosMarvel();
+
+//let NombreDelMarvel = "HULK";
+export async function  buscarMarvelporNombre (NombreDelMarvel){
+try{
+    const data = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=89d89bbcc96a5518b7045154acab2cd0&hash=bcd2343e53b65e1753264d81de0fa5f3&name=${NombreDelMarvel}`);
+
+const dataJson = await data.json()
+const resultadosObtenidos = dataJson.data.results
+const resultadosFormateados = [];
+
+resultadosObtenidos.forEach((marvel) => {
+ let personajeFormateado = {
+ imagen: marvel.thumbnail,
+ name: marvel.name,
+ numero: marvel.id
+
+ };
+
+ resultadosFormateados.push(personajeFormateado);
+})
+
+return resultadosFormateados;
+} catch (error) {
+  console.error('Error en la busqueda del personaje:', error);
+ return [];
+}
+}
+buscarMarvelporNombre();
+//const personajeBuscado = await BuscarMarvelporNombre (NombreDelMarvel);
+//console.log (personajeBuscado);
